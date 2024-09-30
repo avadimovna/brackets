df0c15960a0592e825bea4e6539a34e0745ef721
avadimovna <anastasia.khotyan@gmail.com>
2024-09-30 15:45:30 +0300

    Create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1] && stack[stack.length - 1] !== str[i]) {
+        stack.push(str[i]);
+        continue;
+      } 
+      if (str[i] === bracketsConfig[j][0] && str[i] !== bracketsConfig[j][1]){
+        stack.push(str[i]);
+        continue;
+      }
+      if (str[i] === bracketsConfig[j][1]){
+        if (stack[stack.length - 1] === bracketsConfig[j][0]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return stack.length === 0;
 }
+
+
